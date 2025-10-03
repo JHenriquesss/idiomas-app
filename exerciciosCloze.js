6fe66b3be360ea4692feb0aa7cf05b59078a2026
JHenriquesss <henriquessilvat@gmail.com>
2025-10-03 18:36:46 -0300

    Delete exerciciosCloze.js

--- exerciciosCloze.js
+++
@@ -1,8 +0,0 @@
-const exerciciosClozemaster = [
-  { frase: "I have a ___ dog.", resposta: "big" },
-  { frase: "She is reading a ___.", resposta: "book" },
-  { frase: "The ___ is blue.", resposta: "sky" },
-  { frase: "I drink ___ in the morning.", resposta: "coffee" },
-  { frase: "He lives in a ___ house.", resposta: "small" }
-  // Adicione mais frases!
-];
