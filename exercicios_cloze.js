c3f28fa8458a172a771162ee7581b329a27d5bd7
JHenriquesss <henriquessilvat@gmail.com>
2025-10-03 18:37:03 -0300

    Delete exercicios_cloze.js

--- exercicios_cloze.js
+++
@@ -1,9 +0,0 @@
-const exerciciosCloze = [
-  {
-    frase: "Where are you _____?",
-    opcoes: ["in", "from", "at", "on"],
-    resposta: "from",
-    traducao: "De onde você é?"
-  },
-  // ... (inclua os outros exercícios conforme antes)
-];
